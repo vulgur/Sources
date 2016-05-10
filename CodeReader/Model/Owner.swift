2dd92186c7ddda0d900bd23a9f777d8384203d1c
Wang Shudao <shudao.wang@me.com>
2016-05-10 15:50:01 +0800

    Add models of Owner and Repo

---
+++ CodeReader/Model/Owner.swift
@@ -0,0 +1,27 @@
+//
+//  Owner.swift
+//  CodeReader
+//
+//  Created by vulgur on 16/5/10.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import ObjectMapper
+
+class Owner: Mappable {
+    var name: String?
+    var ownerId: String?
+    var avatarURLString: String?
+    
+    required init?(_ map: Map) {
+        
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        name            <- map["login"]
+        ownerId         <- map["id"]
+        avatarURLString <- map["avatar_url"]
+    }
+    
+}
---
+++ CodeReader/Model/Repo.swift
@@ -0,0 +1,46 @@
+//
+//  Repo.swift
+//  CodeReader
+//
+//  Created by vulgur on 16/5/10.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import ObjectMapper
+
+class Repo: Mappable {
+    
+    var repoId: String?
+    var name: String?
+    var fullName: String?
+    var owner: Owner?
+    var description: String?
+    var size: Int?
+    var starsCount: Int?
+    var watchersCount: Int?
+    var language: String?
+    var forksCount: Int?
+    var createdDate: NSDate?
+    var pushedDate: NSDate?
+    
+    required init?(_ map: Map) {
+    
+    }
+    
+    // Mappable
+    func mapping(map: Map) {
+        repoId          <- map["id"]
+        name            <- map["name"]
+        fullName        <- map["full_name"]
+        owner           <- map["owner"]
+        description     <- map["description"]
+        size            <- map["size"]
+        starsCount      <- map["stargazers_count"]
+        watchersCount   <- map["watchers_count"]
+        language        <- map["language"]
+        forksCount      <- map["forks"]
+        createdDate     <- map["created_at"]
+        pushedDate      <- map["pushed_at"]
+    }
+    
+}
