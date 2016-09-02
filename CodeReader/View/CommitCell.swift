31a5573527a0d8682e66f0e072a4b02b13cc8b59
Wang Shudao <shudao.wang@me.com>
2016-09-03 01:17:21 +0800

    Add CommitCell and design the xib

---
+++ CodeReader/View/CommitCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommitCell.swift
+//  CodeReader
+//
+//  Created by vulgur on 16/9/3.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import UIKit
+
+class CommitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
