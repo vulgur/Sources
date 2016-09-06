ab783830556c871dc259851eadb92854b574f359
Wang Shudao <shudao.wang@me.com>
2016-09-06 17:00:30 +0800

    Add Branch

---
+++ CodeReader/Model/Branch.swift
@@ -0,0 +1,39 @@
+//
+//  Branch.swift
+//  CodeReader
+//
+//  Created by vulgur on 16/9/5.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+struct LatestCommit: Mappable {
+    var sha: String?
+    var URLString: String?
+    
+    init?(_ map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        sha        <- map["sha"]
+        URLString  <- map["url"]
+    }
+}
+
+struct Branch: Mappable {
+    
+    var name: String?
+    var lastestCommit: LatestCommit?
+    
+    init?(_ map: Map) {
+        
+    }
+    
+    mutating func mapping(map: Map) {
+        name            <- map["name"]
+        lastestCommit   <- map["commit"]
+    }
+}
---
+++ CodeReader/ViewModel/CommitListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CommitListViewModel.swift
+//  CodeReader
+//
+//  Created by vulgur on 16/9/5.
+//  Copyright © 2016年 MAD. All rights reserved.
+//
+
+import Foundation
